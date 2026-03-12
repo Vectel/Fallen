3f49be54f763669539e8fe742d2073901dc4e8fc
flixut <flixut@gmail.com>
2026-03-12 13:47:15 +0100

    removed shit

--- Assets/global helper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public static class GlobalHelper 
-{
-    public static string GenerateuniqueID( GameObject obj)
-    {
-        return $"{obj.scene.name}_{obj.tarnsform.position.x}_{obj.position.y}";
-    }
-}
